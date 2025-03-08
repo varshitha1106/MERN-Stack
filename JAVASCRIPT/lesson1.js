aac21dd930ff9a3734fbd6d9c31aaab9c1671588
varshitha1106 <153988952+varshitha1106@users.noreply.github.com>
2025-03-08 15:13:51 +0530

    Add files via upload

---
+++ JAVASCRIPT/lesson1.js
@@ -0,0 +1,37 @@
+//1a
+alert('Good Morning!');
+
+//1b
+alert('Varshitha Poddaturi');
+
+//1c
+10+5
+15
+
+//1d
+20-5
+15
+
+//1e
+2+2-5
+-1
+
+//1f
+document.body.innerHTML='Good morning!';
+'Good morning!'
+
+//1g
+document.body.innerHTML='Varshitha Poddaturi';
+'Varshitha Poddaturi'
+
+//1h
+10+8+20
+38
+
+//1i
+100-20-50+200
+230
+
+//1j
+document.body.innerHTML=' ';
+' '
