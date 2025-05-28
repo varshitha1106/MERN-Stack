b58980df789fcdf7112ae876cb6016bdab6dcc34
varshitha1106 <varshithapoddaturi11@gmail.com>
2025-05-28 20:34:08 +0530

    move addtocart into cart.js

--- JAVASCRIPT/javascript-amazon-project/data/cart.js
+++ JAVASCRIPT/javascript-amazon-project/data/cart.js
@@ -1,1 +1,24 @@
 export const cart=[];  //this variable can be used outside of a file(export)
+
+
+export function addToCart(productId){
+  let matchingItem;
+
+  cart.forEach((cartItem)=>{
+    if(productId===cartItem.productName){
+      matchingItem=cartItem;
+
+    }
+
+  });
+  if(matchingItem){
+    matchingItem.quantity++;
+  }else{
+    cart.push({
+    productId:productId,
+    quantity:1
+    });
+    
+  }
+
+}
--- JAVASCRIPT/javascript-amazon-project/scripts/amazon.js
+++ JAVASCRIPT/javascript-amazon-project/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 let productsHtml='';
 products.forEach((product)=>{
@@ -58,37 +58,23 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').innerHTML=productsHtml;
 
+function updateCartQuantity(){
+  let cartQuantity=0;
+
+  cart.forEach((cartItem)=>{
+    cartQuantity+=cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+}
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=>{
     const productId=button.dataset.productId;
+    addToCart(productId);
+    updateCartQuantity();
+    
 
-    let matchingItem;
-
-    cart.forEach((item)=>{
-      if(productId===item.productName){
-        matchingItem=item;
-
-      }
-
-    });
-    if(matchingItem){
-      matchingItem.quantity++;
-    }else{
-      cart.push({
-      productId:productId,
-      quantity:1
-      });
-      
-    }
-
-    let cartQuantity=0;
-
-
-    cart.forEach((item)=>{
-      cartQuantity+=item.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+    
 
     
   });
