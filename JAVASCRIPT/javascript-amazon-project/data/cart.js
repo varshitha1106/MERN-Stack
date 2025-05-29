91fac273654c227d28679ee84c08728747b9b326
varshitha1106 <varshithapoddaturi11@gmail.com>
2025-05-29 15:53:04 +0530

    make delivery options interactive

--- JAVASCRIPT/javascript-amazon-project/data/cart.js
+++ JAVASCRIPT/javascript-amazon-project/data/cart.js
@@ -24,7 +24,7 @@ export function addToCart(productId){
   let matchingItem;
 
   cart.forEach((cartItem)=>{
-    if(productId===cartItem.productName){
+    if(productId===cartItem.productId){
       matchingItem=cartItem;
 
     }
@@ -53,5 +53,22 @@ export function removeFromCart(productId){
   });
   cart=newCart;
   saveToStorage();
+
+}
+
+export function updateDeliveryOption(productId,deliveryOptionId){
+  let matchingItem;
+
+  cart.forEach((cartItem)=>{
+    if(productId===cartItem.productId){
+      matchingItem=cartItem;
+
+    }
+
+  });
+
+  matchingItem.deliveryOptionId=deliveryOptionId;
+  saveToStorage();
+  
 
 }
--- JAVASCRIPT/javascript-amazon-project/scripts/checkout.js
+++ JAVASCRIPT/javascript-amazon-project/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart,removeFromCart} from '../data/cart.js'; //named export syntax
+import {cart,removeFromCart,updateDeliveryOption} from '../data/cart.js'; //named export syntax
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -97,7 +97,9 @@ function deliveryOptionHTML(matchingProduct,cartItem){
 
     HTML+=
     `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
       <input type="radio" ${isChecked ? 'Checked' : ''}
         class="delivery-option-input"
         name="delivery-option-${matchingProduct.id}">
@@ -130,3 +132,11 @@ document.querySelectorAll('.js-delete-link')
     });
 
   });
+
+  document.querySelectorAll('.js-delivery-option')
+    .forEach((element)=>{
+      element.addEventListener('click',()=>{
+        const {productId,deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId,deliveryOptionId);
+      });
+    });
